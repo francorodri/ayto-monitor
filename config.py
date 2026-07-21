e0b56888b555e804150fe65c474e85622c9b9d1b
Rodrigo Noreña <francorodri@hotmail.com>
2026-07-21 12:20:17 +0000

    Seveth

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ SERVICIOS = [
         "url": (
             "https://citaprevia.ayto-fuenlabrada.es/"
             "qsige.localizador/citaPrevia/"
-            "disponible/centro/34/servicio/1/calendario"
+            "disponible/centro/4/servicio/1/calendario"
         ),
         "enlace": "https://citaprevia.ayto-fuenlabrada.es/citaprevia/#/es/home?uuid=0242a-85e2-064a3-a1eb"
     },
