26eb4b9c6f96353a8458e02b232ea660f684a506
Rodrigo Noreña <francorodri@hotmail.com>
2026-07-21 11:27:45 +0000

    Fourth

--- config.py
+++ config.py
@@ -5,25 +5,36 @@ Añade aquí nuevos servicios si quieres ampliar el seguimiento.
 
 SERVICIOS = [
     {
-        "id": "padron",
-        "nombre": "Padrón",
+        "id": "padron_ayto",
+        "nombre": "Padrón (Altas, Cambios, Modif, Renovación)",
         "centro": "Ayuntamiento de Fuenlabrada",
         "url": (
             "https://citaprevia.ayto-fuenlabrada.es/"
             "qsige.localizador/citaPrevia/"
             "disponible/centro/1/servicio/1/calendario"
         ),
-        "enlace": "https://citaprevia.ayto-fuenlabrada.es/"
+        "enlace": "https://citaprevia.ayto-fuenlabrada.es/citaprevia/#/es/home?uuid=0242a-85e2-064a3-a1eb"
     },
     {
-        "id": "volantes_padron",
-        "nombre": "Volantes Padrón",
-        "centro": "Ayuntamiento de Fuenlabrada",
+        "id": "padron_jmd_loranca",
+        "nombre": "Padrón (Altas, Cambios, Modif, Renovación)",
+        "centro": "JMD Loranca",
         "url": (
             "https://citaprevia.ayto-fuenlabrada.es/"
             "qsige.localizador/citaPrevia/"
-            "disponible/centro/1/servicio/2/calendario"
+            "disponible/centro/3/servicio/1/calendario"
         ),
-        "enlace": "https://citaprevia.ayto-fuenlabrada.es/"
-    }
+        "enlace": "https://citaprevia.ayto-fuenlabrada.es/citaprevia/#/es/home?uuid=0242a-85e2-064a3-a1eb"
+    },
+    {
+        "id": "padron_jmd_vivero",
+        "nombre": "Padrón (Altas, Cambios, Modif, Renovación)",
+        "centro": "JMD Vivero",
+        "url": (
+            "https://citaprevia.ayto-fuenlabrada.es/"
+            "qsige.localizador/citaPrevia/"
+            "disponible/centro/34/servicio/1/calendario"
+        ),
+        "enlace": "https://citaprevia.ayto-fuenlabrada.es/citaprevia/#/es/home?uuid=0242a-85e2-064a3-a1eb"
+    },
 ]
